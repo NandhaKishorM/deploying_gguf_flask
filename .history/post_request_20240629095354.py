9d27f1273a105d387bc2f99f2f0a321f4fb08771
Nandakishor <nandumpilicode@gmail.com>
2024-06-29 10:07:40 +0530

    inital commit

---
+++ .history/post_request_20240629095354.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+url = "http://localhost:5000//api/deployment"
+data = {
+    "message": "test_user"
+}
+
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.post(url, data=json.dumps(data), headers=headers)
+
+print(response.json())
---
+++ .history/post_request_20240629095457.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = "http://localhost:5000//api/deployment"
+data = {
+    "message": "what are the best pesticides for crops in Kerala?"
+}
+
+headers = {
+    'Content-Type': 'application/json'
+}
+
+response = requests.post(url, data=json.dumps(data), headers=headers)
+
+output  =  response.json()
+result = output
